3b5c7993a3023794987373060bc3301b70011aea
krrakshit <rkrkr365@gmail.com>
2024-08-05 10:55:37 +0530

    Create fnproduct.java

---
+++ fnproduct.java
@@ -0,0 +1,13 @@
+public class fnproduct {
+    public static int mult(int a,int b){
+        int prod =a*b;
+        return prod;
+    }
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+       int ans =  mult(a,b);
+        System.out.println(ans);
+    }
+}
