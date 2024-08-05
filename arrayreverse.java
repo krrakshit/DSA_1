8a649955da3b8165f29d0682adfa365babcd9f95
krrakshit <rkrkr365@gmail.com>
2024-08-05 10:51:48 +0530

    Create arrayreverse.java

---
+++ arrayreverse.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class arrayreverse{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of array");
+        int n = sc.nextInt();
+        int []ar = new int[n];
+        System.out.println("enter the elements of array");
+        for (int i = 0;i<ar.length;i++){
+            ar[i] = sc.nextInt();
+        }
+        for (int i = 0;i<ar.length;i++){
+            System.out.print(ar[i]+" ");
+        }
+        System.out.println();
+        int first = 0;int last = ar.length-1;
+        while (first<last){
+            int temp = ar[last];
+            ar[last] = ar[first];
+            ar[first] = temp;
+            first++;
+            last--;
+        }
+        for (int i = 0;i<ar.length;i++){
+            System.out.print(ar[i]+" ");
+        }
+    }
+}
