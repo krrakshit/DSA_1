a5166c205db20cefcae1c7fcd1bd13f93c8ab816
krrakshit <rkrkr365@gmail.com>
2024-08-05 10:55:26 +0530

    Create loopsum.java

---
+++ loopsum.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class loopsum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+       // int sum = 0;
+
+// FOR ASCENDING ORDER PRINT
+
+        for(int num = 1; num <=n; num++) {
+            System.out.println(num);
+      // FOR SUM     sum = sum + num;
+
+        }
+      //  System.out.println("sum of numbers upto n:" + sum);
+
+    }
+}
+
+//import java.util.Scanner;
+//
+//public class loopsum {
+  //  public static void main(String[] args) {
+    //    Scanner sc = new Scanner(System.in);
+      //  int n = sc.nextInt();
+        //// int sum = 0;
+
+
+
+// FOR DESCENDING ORDER PRINT
+//
+
+    //    for(int num = n; num >=1; num--) {
+  //          System.out.println(num);
+      //      //      sum = sum + num;
+
+        //}
+        ////  System.out.println("sum of numbers upto n:" + sum);
+
+   // }
+//}
+
